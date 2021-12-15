b98666e9c5db2dd235b5ecd3be3f1f6aa7cbd2cf
sinhasagar01 <sinhasagar01@gmail.com>
2021-12-15 21:52:53 +0530

    fixed issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Prist | Gatsby & Prismic Starter`,
-        description: `A starter powered by Gatsby and Prismic to showcase portfolios and blogs.`,
-    author: `Marguerite Roth | marguerite.io`,
+    title: `My Portfolio`,
+        description: `My Portfolio`,
+    author: `Sagar Kumar Shrivastava`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
